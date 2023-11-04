df37ce8171aab1a9a6d50e694fdad17f6b910a8c
Carolina Mamani <mamanicarolina27@gmail.com>
2023-11-04 19:27:53 -0300

    04-2

--- JavaScript/e-commerce2022/client/js/index.js
+++ JavaScript/e-commerce2022/client/js/index.js
@@ -1,10 +1,10 @@
 const shopContent = document.getElementById("shopContent");
-const cart = [];
+const cart = [];//Este es nuestro carrito, un array vacio
 
-productos.forEach((product) => {
+productos.forEach((product) =>{
     const content = document.createElement("div");
-    content.innerHTML = `
-    <img src="${product.img}">
+    content.innerHTML = ` 
+    <img src= "${product.img}">
     <h3>${product.productName}</h3>
     <p>${product.price} $</p>
     `;
@@ -15,26 +15,25 @@ productos.forEach((product) => {
 
     content.append(buyButton);
 
-    buyButton.addEventListener("click", () => {
+    buyButton.addEventListener("click", () =>{
         const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
-        
+
         if(repeat){
-          cart.map((prod) => {
-              if(prod.id === product.id){
-                  prod.quanty++;
-                  displayCartCounter();
-              }
-          });
-      } else {
-        cart.push({
+            cart.map((prod)=> {
+                if (prod.id === product.id ){
+                    prod.quanty++;
+                    displayCartCounter();
+             }
+            });
+        }else {
+             cart.push({
             id: product.id,
             productName: product.productName,
             price: product.price,
             quanty: product.quanty,
-            img: product.img,  
+            img: product.img,
             });
             displayCartCounter();
         }
+        });
     });
-});
-
--- JavaScript/e-commerce2022/client/js/products.js
+++ JavaScript/e-commerce2022/client/js/products.js
@@ -1,30 +1,33 @@
 const productos = [
     {
-        id:1,
-        productName: "Banana",
+        id: 1,
+        productName: 'Lapices',
         price: 480,
         quanty: 1,
-        img: "/client/media/banana.jfif",
+        img: "/client/media/lapices.png",
     },
     {
-        id:2,
-        productName: "Leche",
+        id: 2,
+        productName: 'Cuaderno Doble',
         price: 950,
         quanty: 1,
-        img: "/client/media/leche.jfif",
+        img: "/client/media/cuadernoDoble.png",
     },
+
     {
-        id:3,
-        productName: "Pollo",
+
+        id: 3,
+        productName: 'Dos Mochilas',
         price: 750,
-        quanty: 1,
-        img: "/client/media/pollo.jfif",
+        quanty: 2,
+        img: "/client/media/dosMochi.png"
     },
+
     {
-        id:4,
-        productName: "Mayonesa",
-        price: 510,
-        quanty: 1,
-        img: "/client/media/mayonesa.jfif",
-    },
-];
+        id: 4,
+        productName: 'Libros',
+        price: 5000,
+        quanty: 5,
+        img: "/client/media/libros.png"
+    }
+]
