b9912527bdec9b1119854a8a5a1bf8a361a789c8
Caro <mamanicarolina27@gmail.com>
2024-07-17 20:51:45 -0300

    JV 2 Video 3

--- Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -3,8 +3,8 @@ package utn.estudiantes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudiantesApplication {
+@SpringBootApplication //notacion que es para ejecutar
+public class EstudiantesApplication {//la clase
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
