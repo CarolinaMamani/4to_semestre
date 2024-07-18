91084ebf698c937d7c3a340ef301427f253f12d7
Caro <mamanicarolina27@gmail.com>
2024-07-18 20:19:11 -0300

    JV 3 Video 3

--- Java/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
+++ Java/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -4,8 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity //es una clase identidad que representa una tabla de datos
+
+//Boilerplate - codigo repetitivo de
+@Data //(constructores get set)
+@NoArgsConstructor //agrega el constructor vacio
+@AllArgsConstructor //constructor con todos los argumentos (id nombre apellido tel email)
+@ToString //metodo to String
+
 public class Estudiante {
     //agregamos los atributos
 
@@ -13,5 +24,8 @@ public class Estudiante {
     @Id //es para que agregue el numero de la llave primaria
     @GeneratedValue(strategy = GenerationType.IDENTITY) //para llaves autoincreventables
     private Integer idEstudiante;
-
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
 }
