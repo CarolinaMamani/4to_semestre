8ba4de6368e937ba9d63c61b2c7dab175d510f93
Caro <mamanicarolina27@gmail.com>
2024-07-18 20:35:53 -0300

    JV 3 Video 5

--- Java/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
+++ Java/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
@@ -1,7 +1,8 @@
 package utn.estudiantes.repositorio;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import utn.estudiantes.modelo.Estudiante;
-
-public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer >{
+//Nuestro repositorio
+public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer> {
 
 }
