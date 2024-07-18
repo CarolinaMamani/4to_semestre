e0d73ae91b980c21e5cec93a16d95b37c4670458
Caro <mamanicarolina27@gmail.com>
2024-07-18 20:10:21 -0300

    JV 3 Video 2

--- Java/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
+++ Java/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -1,4 +1,17 @@
 package utn.estudiantes.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity //es una clase identidad que representa una tabla de datos
 public class Estudiante {
+    //agregamos los atributos
+
+    //Llave primaria es: idestudiantes
+    @Id //es para que agregue el numero de la llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //para llaves autoincreventables
+    private Integer idEstudiante;
+
 }
