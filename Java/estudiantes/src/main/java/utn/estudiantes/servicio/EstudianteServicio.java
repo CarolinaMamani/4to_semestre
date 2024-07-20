8fa26110e7511454e2ab7e2bb6f7e128c7c112b4
Caro <mamanicarolina27@gmail.com>
2024-07-20 20:50:55 -0300

    JV 4 Video 1

--- Java/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Java/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -1,5 +1,38 @@
 package utn.estudiantes.servicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
+
+import java.util.List;
+
+//notacion de service
+@Service //Componentes de tipo Service
 public class EstudianteServicio implements IEstudianteServicio {
 
+    //Spring es una fabrica de objetos, se encarga de injerctar la dependencia de los objetos
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        return null;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+
+    }
 }
