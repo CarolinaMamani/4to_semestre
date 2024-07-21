98f19decf21cfdf4242dc658cafc33381668d923
Caro <mamanicarolina27@gmail.com>
2024-07-20 22:02:07 -0300

    JV 4 Video 6

--- Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -18,13 +18,18 @@ public class EstudiantesApplication implements CommandLineRunner {//la clase
 	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
 	//el logger es la forma que enviamos informacion a la consola
 
+	String nl = System.lineSeparator();//para todos los sistemas operativos llamamos a lineseprator para que nos reguese el sato de linea
 
 	public static void main(String[] args) {//se ejecuta la fabrica de spring
+		logger.info("Iniciando ... o 3 o");
+		// Levantar la fabrica de Spring
 		SpringApplication.run(EstudiantesApplication.class, args);
+		logger.info("Fin o U o");
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		logger.info("Estamos ejecutando el metodo run de Spring ...");
 
 	}
 }
