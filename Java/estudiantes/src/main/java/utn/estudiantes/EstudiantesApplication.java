ed7882541bb2c0ab38208ee1b1df0174a84271d9
Caro <mamanicarolina27@gmail.com>
2024-07-20 21:24:49 -0300

    JV 4 Video 4

--- Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,19 @@
 package utn.estudiantes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication //notacion que es para ejecutar
-public class EstudiantesApplication {//la clase
+public class EstudiantesApplication implements CommandLineRunner {//la clase
+	//el implements CommandLineRunner (interface) es para ejcutar por consola el menu
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
