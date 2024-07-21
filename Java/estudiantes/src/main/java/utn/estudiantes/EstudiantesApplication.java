6f9b47bac0764a019cbb582124430ccd598c9c75
Caro <mamanicarolina27@gmail.com>
2024-07-20 21:37:35 -0300

    JV 4 Video 5

--- Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,14 +1,25 @@
 package utn.estudiantes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
 
 @SpringBootApplication //notacion que es para ejecutar
 public class EstudiantesApplication implements CommandLineRunner {//la clase
 	//el implements CommandLineRunner (interface) es para ejcutar por consola el menu
 
-	public static void main(String[] args) {
+	@Autowired
+	//definimos el atributo estudiante usando la capa de servicio
+	private EstudianteServicio estudianteServicio;
+	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+	//el logger es la forma que enviamos informacion a la consola
+
+
+	public static void main(String[] args) {//se ejecuta la fabrica de spring
 		SpringApplication.run(EstudiantesApplication.class, args);
 	}
 
