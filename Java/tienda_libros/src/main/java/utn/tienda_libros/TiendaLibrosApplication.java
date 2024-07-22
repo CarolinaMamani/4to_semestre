7c587a79a40638dc3b02d689da8f72b4ac492da9
Caro <mamanicarolina27@gmail.com>
2024-07-22 10:50:44 -0300

    Java 7 Video 1

--- Java/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -3,7 +3,7 @@ package utn.tienda_libros;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//para que se levante spring de manera automatica
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
