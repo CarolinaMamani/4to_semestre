fa6e07eddb05d232382b40116fe9d73f1bab8a3a
Caro <mamanicarolina27@gmail.com>
2024-07-23 12:52:53 -0300

    Java 8 Video 2

--- Java/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,9 +1,13 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroForm;
+
+import java.awt.EventQueue;
 
 @SpringBootApplication//para que se levante spring de manera automatica
 public class TiendaLibrosApplication {
@@ -12,5 +16,13 @@ public class TiendaLibrosApplication {
 		SpringApplication.run(TiendaLibrosApplication.class, args);
 	}
 		ConfigurableApplicationContext contextoSpring =
-				new SpringApplicationBuilder(TiendaLibrosApplication.class);
+				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+		//Ejecutamos el codigo para cargar e formulario
+		EventQueue.invokeLater(() -> {
+			//Obtenemos el objeto form atravez del spring
+		LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+	} )
 }
