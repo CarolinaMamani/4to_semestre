7baf458972929a25f1dc4a4b47f39984c0a77ee2
Caro <mamanicarolina27@gmail.com>
2024-07-23 21:12:39 -0300

    Java 9 Video 5 y 6

--- Java/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
+++ Java/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
@@ -2,6 +2,7 @@ package utn.tienda_libros.vista;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import utn.tienda_libros.modelo.Libro;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
@@ -45,8 +46,36 @@ public class LibroForm extends JFrame {
     private void agregarLibro(){
         //Leer los valores del formulario
         if (libroTexto.getText().equals("")){ //si es true que la caja esta vacia
-            mostrarMensaje("Ingresa el nombre del libro: ")
+            mostrarMensaje("Ingresa el nombre del libro: ");
+            libroTexto.requestFocusInWindow();//mueve el cursor para que se vuelva a ingresar el nombre del libro
+            return;
         }
+        //else
+        var nombre = libroTexto.getText();
+        var autor = autorTexto.getText();
+        var precio = Double.parseDouble(precioTexto.getText());
+        var existencias = Integer.parseInt(existenciasTexto.getText());
+        //Creamos el objeto libro
+        var libro = new Libro(null,nombre,autor,precio,existencias);
+        //libro.setNombre(nombre);
+        //libro.setAutor(autor);
+        //libro.setPrecio(precio);
+        //libro.setExistencias(existencias);
+        this.libroServicio.guardarLibro(libro);
+        mostrarMensaje("Agregado! ");
+        limpiarFormulario();
+        listarLibros();
+    }
+
+    private void limpiarFormulario(){
+        libroTexto.setText("");//la caja queda limpia
+        autorTexto.setText("");
+        precioTexto.setText("");
+        existenciasTexto.setText("");
+    }
+
+    private void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(this, mensaje);
     }
 
 
