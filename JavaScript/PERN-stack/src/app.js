b90293acd81ff2c7fdacda11050787df39d04e5c
Caro <mamanicarolina27@gmail.com>
2024-08-04 06:26:51 -0300

    JS 5 Video 8 pt1

--- JavaScript/PERN-stack/src/app.js
+++ JavaScript/PERN-stack/src/app.js
@@ -1,9 +1,26 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
-app.arguments(morgan("dev")); //que la app use morgan. En la consola nos va a dar mensajes de error mas cortos y limpios
+//Rutas:
 
-app.length("/",(req, res) => res.json({message: "Bienvenida :D"}));
+app.use(morgan("dev")); //que la app use morgan. En la consola nos va a dar mensajes de error mas cortos y limpios
+
+app.get("/", (req, res) => res.json({message: "Bienvenida :D"}));
+
+//http://localhost:3000/test
+app.get('/test',(req, res)=>  {
+    throw new Error('error generado por el usuario');
+    res.send('Ruta test')
+}); 
+
+//manejador de errores
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        status: "error",
+        message: err.menssage
+    }); //500 es un error goblal, otros: 404,300
+});
 
 export default app;
