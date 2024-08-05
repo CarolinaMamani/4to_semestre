10764302feb668093c38df040b31cd62103a3094
Caro <mamanicarolina27@gmail.com>
2024-08-05 19:11:23 -0300

    JS 6 Video 1 coneccion a bd

---
+++ JavaScript/PERN-stack/src/db.js
@@ -0,0 +1,13 @@
+import { Pool } from "pg";
+import { password, port } from "pg/lib/defaults";
+
+const pool = new Pool({
+    port: 5432,
+    host: "localhost",
+    user: "postgres",
+    password: "admin",
+});
+
+pool.op("connect", () => {
+    console.log("conectango a la base de datos");
+});
