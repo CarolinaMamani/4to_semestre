48539cdbf7584d9d7b43ad1923a020f0a3182329
Caro <mamanicarolina27@gmail.com>
2024-08-08 08:39:38 -0300

    JS 6 Video 3

--- JavaScript/PERN-stack/src/controllers/tareas.controller.js
+++ JavaScript/PERN-stack/src/controllers/tareas.controller.js
@@ -1,13 +1,23 @@
-
+import { pool } from "../db.js";
 
 export const listarTareas =  (req,res) => res.send('obteniendo tareas');
 
 export const listarTarea = (req,res)=> res.send('obteniendo una tarea unica');
 
-export const crearTarea = (req, res)=> {
+export const crearTarea = async (req, res)=> {
     //verificamos que este funcionando para el cliente
-    console.log(req.body);
+    const {titulo, descripcion} = req.body;
+    
+    //para que no se caiga el servidos agregamos un try catch para el manejo de errores
+
+    try{  const {rows} = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2)', [titulo, descripcion]);
+    console.log(rows);
     res.send('creando tarea');
+    } catch (error) {
+        console.log('algo salio mal');
+    }
+
+
 };
 
 export const actualizarTarea = (req,res) => res.send('actualizando tarea unica');
--- JavaScript/PERN-stack/src/db.js
+++ JavaScript/PERN-stack/src/db.js
@@ -1,10 +1,12 @@
 import pg from "pg";
 
+
 export const pool = new pg.Pool({
     port: 5432,
     host: "localhost",
     user: "postgres",
     password: "admin",
+    database: "PERN",
 });
 
 pool.on("connect", () => {
