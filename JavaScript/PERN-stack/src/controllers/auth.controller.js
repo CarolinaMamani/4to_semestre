f2fa551dfb5d1aa430c65d0f9ae8353755accc0f
Caro <mamanicarolina27@gmail.com>
2024-08-15 10:20:52 -0300

    JS 7 Video 1 pt1

--- JavaScript/PERN-stack/src/controllers/auth.controller.js
+++ JavaScript/PERN-stack/src/controllers/auth.controller.js
@@ -1,6 +1,13 @@
+import {pool} from "../db.js"
+
 export const signin = (req,res) => res.send("ingresando");
 
-export const signup = (req,res) => res.send('registrando');
+export const signup = (req,res) => {
+    const {name, email, password} = req.body;
+    res.send("Registrando");
+
+    pool.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3)", [name, email, password])
+};
 
 export const signout = (req,res) => res.send('cerrando secion');
 
