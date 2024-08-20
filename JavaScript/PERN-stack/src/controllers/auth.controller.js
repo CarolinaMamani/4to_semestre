648e6548e5698a84aed339748d43736730d653b4
Caro <mamanicarolina27@gmail.com>
2024-08-20 17:02:01 -0300

    JS 8 Video 3

--- JavaScript/PERN-stack/src/controllers/auth.controller.js
+++ JavaScript/PERN-stack/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import {pool} from "../db.js"
 import bcrypt from "bcrypt";
 import {createAccessToken} from "../libs/jwt.js"
+import md5 from "md5";
 
 export const signin = async(req,res) => {
     const {email, password} = req.body;
@@ -33,9 +34,9 @@ export const signup = async(req,res, next) => {
     
     try {
         const hashedPassword = await bcrypt.hash(password,10);
-        console.log(hashedPassword);
+        const gravatar = "https://www.gravatar.com/avatar/" + md5(email);
 
-        const result =  await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) Returning *", [name, email, hashedPassword]);
+        const result =  await pool.query("INSERT INTO usuarios (name, email, password, gravatar) VALUES ($1, $2, $3, $4) Returning *", [name, email, hashedPassword, gravatar]);
 
         const token = await createAccessToken({id:result.rows[0].id});
         console.log(result);
