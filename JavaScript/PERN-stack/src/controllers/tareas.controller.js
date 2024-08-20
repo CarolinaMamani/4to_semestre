5f91aab7664b451c2727eb1e71eb8c8cc4135448
Caro <mamanicarolina27@gmail.com>
2024-08-20 16:40:49 -0300

    JS 8 Video 3 pt2

--- JavaScript/PERN-stack/src/controllers/tareas.controller.js
+++ JavaScript/PERN-stack/src/controllers/tareas.controller.js
@@ -2,7 +2,7 @@ import { pool } from "../db.js";
 
 export const listarTareas =  async(req,res) => {
         console.log(req.usuarioId);
-        const resultado = await pool.query('SELECT * FROM tareas');
+        const resultado = await pool.query('SELECT * FROM tareas WHERE usuario_id = $1', [req.usuarioId]);
         
         return res.json(resultado.rows);
     } 
@@ -25,7 +25,7 @@ export const crearTarea = async (req, res, next)=> {
 
     try{  
         
-        const result = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *', [titulo, descripcion]);
+        const result = await pool.query('INSERT INTO tareas (titulo, descripcion, usuario_id) VALUES ($1, $2, $3) RETURNING *', [titulo, descripcion, req.usuarioId]);
         res.json(result.rows[0]); //devuelve una lista en el json
         console.log(result.rows[0]); //devuelve la lista en la consola terminal
     } catch (error) {
