b065fd8fc40d9ffc2e96f03b5f21ba98fa90b3a7
Caro <mamanicarolina27@gmail.com>
2024-08-20 09:44:09 -0300

    JS 7 Video 9

--- JavaScript/PERN-stack/src/router/tareas.routes.js
+++ JavaScript/PERN-stack/src/router/tareas.routes.js
@@ -1,11 +1,11 @@
 import Router from "express-promise-router";
 import { actualizarTarea, crearTarea, eliminarTarea, listarTarea, listarTareas } from "../controllers/tareas.controller.js";
-
+import {isAuth} from "../middlewares/auth.middlewere.js"
 
 const router = Router();
 
 //obtenemos un listado de tareas
-router.get('/tareas', listarTareas); //cambiamos las funciones al achivo
+router.get('/tareas', isAuth, listarTareas); //cambiamos las funciones al achivo
 //tareas.controller.js
 
 //una unica terea
