c74a3065282bf7714143311bc003f0994697e431
Caro <mamanicarolina27@gmail.com>
2024-08-21 10:58:36 -0300

    JS 8 Video 5 pt1

--- JavaScript/PERN-stack/frontend/src/pages/RegisterPage.jsx
+++ JavaScript/PERN-stack/frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Input from '../componentes/ui/imput'
 
 function RegisterPage() {
   return (
-    <h3 className='text-2xl font-bold text-yellow-200'>Registroo</h3>
-  )
+    <div>
+      <h3 className='text-2xl font-bold text-blue-400'>Registro</h3>
+      <form>
+        <Input placeholder="ingrese su nombre"></Input>
+        <Input type="email" placeholder="ingrese su email"></Input>
+        <Input type="password" placeholder="ingrese su contrasenia"></Input>
+        
+
+        <button>Registrarse</button>
+      </form>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
