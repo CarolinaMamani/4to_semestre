ae216c2e30cf41cfebb78bf980779e98f32dd325
Caro <mamanicarolina27@gmail.com>
2024-08-21 10:10:03 -0300

    JS 8 Video 4 pt2

--- JavaScript/PERN-stack/frontend/src/App.jsx
+++ JavaScript/PERN-stack/frontend/src/App.jsx
@@ -1,12 +1,26 @@
 import { Routes, Route } from "react-router-dom"
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import LoginPage from './pages/LoginPage'
+import TareaFormPage from "./pages/TareaFormPage"
+import RegisterPage from './pages/RegisterPage'
+import ProfilePage from './pages/ProfilePage'
+import TareasPage from './pages/TareasPage'
 
 function App(){
   return (
     //creamos diferentes rutas
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/about" element ={<h1>About</h1>} />    
-      </Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/about" element ={<AboutPage/>} />
+      <Route path="/login" element={<LoginPage/>}/> 
+      <Route path="/register" element={<RegisterPage/>}/>
+      <Route path="/perfil" element={<ProfilePage/>}/>
+      <Route path="/tareas" element={<TareasPage/>}/>
+      <Route path="/tareas/crear" element={<TareaFormPage/>}/>
+      
+        
+    </Routes>
   )
 }
 
