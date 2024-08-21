19af3f16f03cf63267a858fded7a4b8765b48c89
Caro <mamanicarolina27@gmail.com>
2024-08-21 11:36:04 -0300

    JS 8 video 5

--- JavaScript/PERN-stack/frontend/src/pages/RegisterPage.jsx
+++ JavaScript/PERN-stack/frontend/src/pages/RegisterPage.jsx
@@ -1,17 +1,23 @@
-import Input from '../componentes/ui/imput'
+import Input from '../componentes/ui/input';
+import { Card } from "../componentes/ui/Card";
+import { Button } from "../componentes/ui/Button";
 
 function RegisterPage() {
   return (
-    <div>
-      <h3 className='text-2xl font-bold text-blue-400'>Registro</h3>
-      <form>
-        <Input placeholder="ingrese su nombre"></Input>
-        <Input type="email" placeholder="ingrese su email"></Input>
-        <Input type="password" placeholder="ingrese su contrasenia"></Input>
-        
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+      
+      <Card>
+        <h3 className='text-2xl font-bold text-blue-400'>Registro</h3>
+        <form>
+          
+          <Input placeholder="ingrese su nombre"></Input>
+          <Input type="email" placeholder="ingrese su email"></Input>
+          <Input type="password" placeholder="ingrese su contrasenia"></Input>
+          
 
-        <button>Registrarse</button>
-      </form>
+          <Button>Registrarse</Button>
+        </form>
+      </Card>
     </div>
   );
 }
