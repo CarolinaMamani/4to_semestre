59a8474b38d8b1c588d96baaedc269b7fef7eb54
Caro <mamanicarolina27@gmail.com>
2024-08-22 16:25:51 -0300

    JS 9 Video 2 pt2

--- JavaScript/PERN-stack/frontend/src/pages/LoginPage.jsx
+++ JavaScript/PERN-stack/frontend/src/pages/LoginPage.jsx
@@ -4,7 +4,12 @@ function LoginPage() {
   return (
     <div className="h-[calc(100vh-64px)] flex items-center justify-center">
       <Card>
-        <h1 className=""></h1>
+        <h1 className="text4x1 font-bold my-2 text-center">Iniciar sesion</h1>
+        <form>
+          <Input placeholder = "Ingrese su email"></Input>
+          <Input placeholder = "Ingrese su contrasenia"></Input>
+          <Button>Ingresar</Button>
+        </form>
       </Card>
 
     </div>
---
+++ JavaScript/PERN-stack/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { Card } from '../componentes/ui'
+
+function NotFound() {
+  return (
+    <div className='h-[calc{100vh-64px)] flex justify-center items-center flex-col'>
+        <Card></Card>
+    </div>
+  )
+}
+
+export default NotFound
