060cd3fdf3896fc9a7cde853eba3f203c1c95b82
Caro <mamanicarolina27@gmail.com>
2024-08-23 22:34:35 -0300

    JS 9 Video 2 final

--- JavaScript/PERN-stack/frontend/src/pages/LoginPage.jsx
+++ JavaScript/PERN-stack/frontend/src/pages/LoginPage.jsx
@@ -1,16 +1,37 @@
 import {Link} from "react-router-dom"
 import {Card, Input, Button, Label} from "../componentes/ui"
+import { useForm } from "react-hook-form";
 
 function LoginPage() {
+  const { register, handleSubmit} = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+      console.log(data);
+  });
   return (
     <div className="h-[calc(100vh-64px)] flex items-center justify-center">
       <Card>
-        <h1 className="text4x1 font-bold my-2 text-center">Iniciar sesion</h1>
-        <form>
+        <h1 className="text-4xl font-bold my-2 text-center">Iniciar sesion</h1>
+        <form onSubmit={onSubmit}>
+
           <Label htmlFor="email">Email</Label>
-          <Input placeholder = "Ingrese su email"></Input>
+          <Input 
+            type="email" 
+            placeholder = "Ingrese su email" 
+            {...register("email", {
+              required: true,
+            })}>
+          </Input>
+
           <Label htmlFor="password">Contrasenia</Label>
-          <Input placeholder = "Ingrese su contrasenia"></Input>
+          <Input 
+            type="password" 
+            placeholder = "Ingrese su contrasenia"
+            {...register("password", {
+              required: true,
+            })}>
+          </Input>
+
           <Button>Ingresar</Button>
         </form>
         <div className="flex justify-between my-4">
