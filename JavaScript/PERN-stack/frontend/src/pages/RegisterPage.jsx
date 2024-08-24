b195dca144477b708bdd38e07ef0614c62863657
Caro <mamanicarolina27@gmail.com>
2024-08-23 22:22:42 -0300

    JS 9 Video 2 pt5

--- JavaScript/PERN-stack/frontend/src/pages/RegisterPage.jsx
+++ JavaScript/PERN-stack/frontend/src/pages/RegisterPage.jsx
@@ -1,6 +1,8 @@
 import Input from '../componentes/ui/input';
 import { Card } from "../componentes/ui/Card";
 import { Button } from "../componentes/ui/Button";
+import { Label } from '../componentes/ui/Label';
+import { Link } from 'react-router-dom';
 //import { Button, Card, Input } from "../componentes/ui";
 import { useForm }from 'react-hook-form';
 import axios from "axios"
@@ -22,19 +24,21 @@ function RegisterPage() {
     <div className="h-[calc(100vh-64px)] flex items-center justify-center">
       
       <Card>
-        <h3 className='text-2xl font-bold text-blue-400'>Registro</h3>
+        <h3 className='text-4xl font-bold text-blue-400 my-2'>Registro</h3>
         <form onSubmit={onSubmit}>
-          
+          <Label htmlFor="name">Nombre</Label>
           <Input placeholder="ingrese su nombre"
           {...register("name", {required: true})}></Input>
           {
             errors.name && <p className="text-red-500">Este campo es requerido</p>
           }
-          <Input type="email" placeholder="ingrese su email"
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" placeholder="ingrese su email"s
           {...register("email", {required: true})}></Input>
           {
             errors.email && <p className="text-red-500">Este campo es requerido</p>
           }
+          <Label htmlFor="password">Contrasenia</Label>
           <Input type="password" placeholder="ingrese su contrasenia"
           {...register("password", {required: true})}></Input>
           {
@@ -44,6 +48,10 @@ function RegisterPage() {
 
           <Button>Registrarse</Button>
         </form>
+        <div className="flex justify-between my-4">
+          <p>Ya tienes una cuenta?</p>
+          <Link to="/login"> Iniciar Sesion</Link>
+        </div>
       </Card>
     </div>
   );
