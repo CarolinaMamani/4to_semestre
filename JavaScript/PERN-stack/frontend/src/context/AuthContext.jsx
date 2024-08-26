45a2fda8c8c6cf7a47cd98a2b30c0924c18cff8c
Caro <mamanicarolina27@gmail.com>
2024-08-25 23:00:04 -0300

    JS 9 Video 4 pt1

--- JavaScript/PERN-stack/frontend/src/context/AuthContext.jsx
+++ JavaScript/PERN-stack/frontend/src/context/AuthContext.jsx
@@ -7,12 +7,15 @@ export const useAuth = () => {
         throw new Error("useAuth deber ser usado con AuthProvider ");
         
     }
+    return context;
 }
 
 export function AuthProvider ({children}){
     const [user, setUser] = useState(null);
     const [isAuth, setIsAuth] = useState(false);
     const [errors, setErrors] = useState(null);
+
+    
     return <AuthContext.Provider value={{
         user,
         isAuth,
