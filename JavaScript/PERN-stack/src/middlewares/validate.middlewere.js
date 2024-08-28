3cbb4670482eb41345b607a715bb13e930916e95
Caro <mamanicarolina27@gmail.com>
2024-08-28 14:47:44 -0300

    JS 9 Video 17 pt4

--- JavaScript/PERN-stack/src/middlewares/validate.middlewere.js
+++ JavaScript/PERN-stack/src/middlewares/validate.middlewere.js
@@ -5,6 +5,9 @@ export const validateSchema = (schema) => async (req, res, next) =>{
         next()
     } catch (error) {
         console.log(error);
-        return res.status(400).json(error.errors)
+        if (Array.isArray(error.errors)) {
+            return res.status(400).json(error.errors.map((error) => error.message));
+        }
+        
     }
 };
--- JavaScript/PERN-stack/src/schemas/tareas.schema.js
+++ JavaScript/PERN-stack/src/schemas/tareas.schema.js
@@ -1,8 +1,22 @@
 import {z} from 'zod'
 
 export const createTareasSchema = z.object({
-    titulo: z.string().min(1).max(255),
-    descripcion: z.string().min(1).max(255),
+    titulo: z.string({
+        required_error: "El titulo es requerido",
+        invalid_type_error: "el titulo debe ser un string",
+    }).min(1,{
+        message: "el titulo debe tener almenos 1 caracter"
+    }).max(255, {
+        message: "el titulo debe tenr como maximo 255 caracteres"
+    }),
+    descripcion: z.string({
+        required_error: "descripcion requerida",
+        invalid_type_error: "la descripcion debe ser un string",
+    }).min(0, {
+        message: "la descripcion debe tener almenos 1 string"
+    }).max(255, {
+        message: "la descripcion debe tener como maximo 255 caracteres"
+    }).optional(),
 });
 
 //createTareasSchema.parse({ titulo})
