3e81aca475ccbaf8d332320b634e32b2a1acf7fd
Caro <mamanicarolina27@gmail.com>
2024-08-29 08:21:12 -0300

    JS9.17.8

--- JavaScript/PERN-stack/src/router/auth.routes.js
+++ JavaScript/PERN-stack/src/router/auth.routes.js
@@ -1,16 +1,17 @@
 import Router from "express-promise-router";
 import { profile, signin, signout, signup } from "../controllers/auth.controller.js";
 import { isAuth } from "../middlewares/auth.middlewere.js";
-
+import { validateSchema } from "../middlewares/validate.middlewere.js";
+import { signinSchema, signupSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 //Inicios de secion
 
-router.post('/signin', signin);
+router.post('/signin', validateSchema(signin),signin);
 
-router.post('/signup', signup);
+router.post('/signup', validateSchema(signup), signup);
 
-router.post('/signout', isAuth, signout); //sera con validacion de tokens
+router.post('/signout', signout); //sera con validacion de tokens
 
 router.get('/profile', isAuth, profile);
 
