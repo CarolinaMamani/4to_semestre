8652b93ff02fcc18e8325014c8d8b0aad7dc567e
Caro <mamanicarolina27@gmail.com>
2024-09-03 16:20:36 -0300

    JS9.20.7

--- JavaScript/PERN-stack/frontend/src/context/AuthContext.jsx
+++ JavaScript/PERN-stack/frontend/src/context/AuthContext.jsx
@@ -1,80 +1,92 @@
-import { createContext, useContext, useState, useEffect } from "react"
-import Cookie from 'js-cookie'
+import { createContext, useState, useContext ,useEffect } from "react";
+import Cookie from "js-cookie";
 import axios from "../api/axios";
 
 export const AuthContext = createContext();
 
 export const useAuth = () => {
-    const context =  useContext(AuthContext);
-    if (!context){
-        throw new Error("useAuth deber ser usado con AuthProvider ");
-        
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+  return context;
+};
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [isAuth, setIsAuth] = useState(false);
+  const [errors, setErrors] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const signin = async (data) => {
+    try {
+      const res = await axios.post("/signin", data);
+
+      setUser(res.data);
+      setIsAuth(true);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      if (Array.isArray(error.response.data)) {
+        return setErrors(error.response.data);
+      }
+      setErrors([error.response.data.message]);
     }
-    return context;
-}
+  };
 
-export function AuthProvider ({children}){
-    const [user, setUser] = useState(null);
-    const [isAuth, setIsAuth] = useState(false);
-    const [errors, setErrors] = useState(null);
+  const signup = async (data) => {
+    try {
+      const res = await axios.post("/signup", data);
+      setUser(res.data);
+      setIsAuth(true);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      if (Array.isArray(error.response.data)) {
+        return setErrors(error.response.data);
+      }
+      setErrors([error.response.data.message]);
+    }
+  };
 
-    const signin = async(data) => {
-        try {
-            const res = await axios.post("/signin", data);                
-        setUser(res.data);
-        setIsAuth(true);
-        return res.data;
-
-        } catch (error) {
-        console.log(error);
-        if(Array.isArray(error.response.data)){
-            return setErrors(error.response.data);
-        }
-        setErrors([error.response.data.message])
-        }
-    };
-
-    const signup = async (data) => {
-        try {
-            const res = await axios.post("/signup", data);                
-                setUser(res.data);
-                setIsAuth(true);
-            return res.data;
-
-        } catch (error) {
-            console.log(error)
-            if(Array.isArray(error.response.data)){
-                return setErrors(error.response.data)
-            }
-            setErrors([error.response.data.message])
-        }
-    };
+  const signout = async() => {
+    const res = await axios.post("/signout");
+    setUser(null);
+    setIsAuth(false);
+    return res.data;
+  }
 
     useEffect(() => {
-        if(Cookie.get("token")){
+      setLoading(true);
+        if (Cookie.get("token")) {
             axios.get("/profile").then((res) => {
                 setUser(res.data);
                 setIsAuth(true);
+                setLoading(false);
             }).catch((error) => {
                 setUser(null);
                 setIsAuth(false);
+                setLoading(false);
                 console.log(error);
-            })
-
+            });
         }
+        setLoading(false);
     }, []);
-    
-    return (
-    <AuthContext.Provider value={{
+
+  return (
+    <AuthContext.Provider
+      value={{
         user,
         isAuth,
         errors,
         signup,
         setUser,
         signin,
-        
-    }}>
-        {children}
+        signout,
+        loading,
+      }}
+    >
+      {children}
     </AuthContext.Provider>
-    );
+  );
 }
